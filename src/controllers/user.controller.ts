e3990d66d69dc56e9ebb309ecd874b4075c329e8
Namanjs <namanagarwal686@gmail.com>
2026-03-28 20:08:28 +0530

    changed the registercontoller so the validation is done through zod and removed the manual validation checks

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -3,64 +3,73 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import User from "../models/user.model.js";
-import { Role } from "../models/user.model.js";
+import { loginSchema, login, register, registerSchema } from "../validators/user.validator.js";
 
-export const registerUser = asyncHandler(async (req: Request, res: Response) => {
-    const { username, email, password, role } = req.body;
-
-    const requiredFields = [username, email, password, role];
-    const emptyFields: boolean = requiredFields.some((field) => {
-        return field === "";
-    });
-
-    if (emptyFields) {
-        throw new ApiError(400, "All Fields are required.");
-    }
-
-    if (username.length <= 3 || username.length > 15) {
-        throw new ApiError(400, "Username must be atleast 4 characters and atmost 15 character long.");
-    }
-
-    const userRegex = /^[a-zA-Z][a-zA-Z0-9]*$/;
-    if (!userRegex.test(username)) {
-        throw new ApiError(400, "Username not allowed.")
-    };
-
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(email)) {
-        throw new ApiError(400, "Email not allowed.");
-    };
-
-    if (password.length < 12) {
-        throw new ApiError(400, "Password is too short.")
-    }
-
-    if (!Object.values(Role).includes(role)) {
-        throw new ApiError(400, "Role doesn't exist.")
-    }
+const registerUser = asyncHandler(async (req: Request, res: Response) => {
+    const validateData: register = registerSchema.parse(req.body);
+    const { username, email, password, role } = validateData;
 
     const isDuplicate = await User.findOne({
-        email: email
+        $or: [{email}]
     });
-    if (isDuplicate || null) {
-        throw new ApiError(409, "User with same email already exist.")
-    }
+
+    if(isDuplicate){
+        throw new ApiError(409, "User with email already exist.")
+    };
 
     const user = await User.create({
-        username: username.toLowerCase(),
+        username: username,
         email: email,
         password: password,
-        role: role
+        role: role,
+        createdAt: Date.now()
     });
 
-    const createduser = await User.findById(user._id).select("-password");
+    const createdUser = await User.findById(user._id).select("-password");
 
-    if (!createduser) {
-        throw new ApiError(500, "Something went wrong while registering the user.");
+    if(!createdUser){
+        throw new ApiError(500, "Something went wrong while registering the user.")
     };
 
     return res.status(201).json(
-        new ApiResponse(201, createduser, "User successfully registered.")
+        new ApiResponse(201, createdUser, "User successfully created.")
     )
 });
 
+const loginUser = asyncHandler(async (req: Request, res: Response) => {
+    const validateData: login = loginSchema.parse(req.body);
+    const { username, email, password } = validateData;
+
+    if (!username && !email) {
+        throw new ApiError(400, "Username or email is required to login.");
+    };
+
+    const user = await User.findOne({
+        $or: [
+            ...(username ? [{ username }] : []),
+            ...(email ? [{ email }] : [])
+        ]
+    });
+
+    if (!user) {
+        throw new ApiError(404, "User with username or email does not exist.");
+    };
+
+    const isPasswordCorrect = await user.isPasswordCorrect(password);
+
+    if (!isPasswordCorrect) {
+        throw new ApiError(401, "Password is incorrect.");
+    };
+
+    const userResponse = user.toObject();
+    delete (userResponse as any).password;
+
+    return res.status(200).json(
+        new ApiResponse(200, userResponse, "User logged in successfully.")
+    );
+});
+
+export{
+    registerUser,
+    loginUser
+}
---
+++ src/validators/user.validator.ts
@@ -0,0 +1,32 @@
+import { z } from 'zod';
+import { Role } from '../models/user.model.js';
+
+const loginSchema = z.object({
+    email: z.string().optional(),
+    username: z.string().optional(),
+    password: z.string()
+});
+
+type login = z.infer<typeof loginSchema>
+
+const registerSchema = z.object({
+    username: z
+    .string()
+    .max(15, "Username must be at most 15 characters long.")
+    .min(3, "Username must be atleast 3 characters long.")
+    .regex(/^[a-zA-Z][a-zA-Z0-9_]*$/, "Invalid username format"),
+    email: z.string().email("Invalid email format."),
+    password: z
+    .string()
+    .min(8, "Password must be atleast 8 characters long."),
+    role: z.enum([Role.BUYER, Role.ORGANIZER])
+});
+
+type register = z.infer<typeof registerSchema>
+
+export {
+    login,
+    loginSchema,
+    register,
+    registerSchema
+}
